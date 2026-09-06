bfcd488b4c2f23d5b6b12c87dce4ae05be8b5410
tsukishima-yuji <yuthandarkyaw.y@kkumail.com>
2026-09-06 21:51:22 +0700

    Adding Files & Folders

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "./components/Auth/LoginForm";
+import GrowthChart from "./components/GrowthTracking/GrowthChart";
+
+function App() {
+  return (
+    <div>
+      <h1>Growth Monitoring App</h1>
+
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Auth/LoginForm.jsx
@@ -0,0 +1,5 @@
+function LoginForm() {
+  return <h2>Login Form</h2>;
+}
+
+export default LoginForm;
