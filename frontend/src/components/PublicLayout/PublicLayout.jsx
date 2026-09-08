55460340191472a4e5ea269bc68487d29d4f9a5f
tsukishima-yuji <yuthandarkyaw.y@kkumail.com>
2026-09-08 19:00:08 +0700

    footer fix

--- frontend/src/components/PublicLayout/PublicLayout.jsx
+++ frontend/src/components/PublicLayout/PublicLayout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import PublicNavbar from "./PublicNavbar";
 import PublicFooter from "./PublicFooter";
+import "./PublicLayout.css";
 
 function PublicLayout() {
   return (
-    <>
+    <div className="public-layout">
       <PublicNavbar />
-      <Outlet />
+      <div className="public-layout__content">
+        <Outlet />
+      </div>
       <PublicFooter />
-    </>
+    </div>
   );
 }
 
