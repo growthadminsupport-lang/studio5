f5b9eba78e828e8bad51535254fd66c4f0209951
tsukishima-yuji <yuthandarkyaw.y@kkumail.com>
2026-09-09 00:15:23 +0700

    log in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { NotificationsProvider } from "./context/NotificationsContext";
-import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -8,7 +12,9 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <AuthProvider>
         <NotificationsProvider>
-          <App />
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
         </NotificationsProvider>
       </AuthProvider>
     </BrowserRouter>
