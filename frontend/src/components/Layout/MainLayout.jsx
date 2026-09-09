bdc6f6c0a375eb7b8297f640713fda8fb3d4e406
tsukishima-yuji <yuthandarkyaw.y@kkumail.com>
2026-09-09 22:28:56 +0700

    layout

--- frontend/src/components/Layout/MainLayout.jsx
+++ frontend/src/components/Layout/MainLayout.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import "./MainLayout.css";
 
 function MainLayout() {
   return (
-    <>
+    <div className="main-layout">
       <Navbar />
+
       <main className="page-content">
         <Outlet />
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
