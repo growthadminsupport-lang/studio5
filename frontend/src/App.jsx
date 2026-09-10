4ea7be017fd15e4db13ca8e47cfd4ca59179c2f6
tsukishima-yuji <yuthandarkyaw.y@kkumail.com>
2026-09-10 14:09:51 +0700

    set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import TermsOfUsePage from "./pages/TermsOfUsePage";
 import ProtectedRoute from "./components/Auth/ProtectedRoute";
 import NotificationsPage from "./pages/NotificationsPage";
 import ArticlePage from "./pages/ArticlePage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
   return (
